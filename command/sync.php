7d354ed7e4e9b40ab6a7cafeaa52b8be629ef9e3
Robin Appelman <icewind@owncloud.com>
2015-07-13 16:13:25 +0200

    split sync handle

--- command/sync.php
+++ command/sync.php
@@ -21,6 +21,7 @@
 namespace OCA\Files_External_Cache\Command;
 
 use OC\Command\FileAccess;
+use OC\Files\Storage\Storage;
 use OCA\Files_External_Cache\Cache\Expirer;
 use OCA\Files_External_Cache\Wrapper\SyncOnionWrapper;
 use OCP\Command\ICommand;
@@ -70,13 +71,16 @@ class Sync implements ICommand {
 		$this->mtime = $mtime;
 	}
 
-	public function handle() {
+	/**
+	 * @return Storage[]
+	 */
+	private function getStorages() {
 		\OC_Util::setupFS($this->userId);
 		$sourceStorage = $this->getStorageById($this->sourceStorageId);
 		$targetStorage = $this->getStorageById($this->targetStorageId);
 
 		if (is_null($targetStorage)) {
-			return;
+			return [null, null];
 		}
 
 		if ($targetStorage->instanceOfStorage('OCA\Files_External_Cache\Wrapper\SyncOnionWrapper') and !$sourceStorage) {
@@ -84,6 +88,11 @@ class Sync implements ICommand {
 			$sourceStorage = $targetStorage->getCacheStorage();
 			$targetStorage = $targetStorage->getBackendStorage();
 		}
+		return [$sourceStorage, $targetStorage];
+	}
+
+	public function handle() {
+		list($sourceStorage, $targetStorage) = $this->getStorages();
 
 		if (is_null($sourceStorage) or
 			is_null($targetStorage) or
